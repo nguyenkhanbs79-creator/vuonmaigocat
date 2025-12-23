facacc2af0bcc155a279323213d76a56ca186a75
nguyenkhanbs79-creator <nguyenkhanbs79@gmail.com>
2025-12-23 14:45:52 +0700

    Remove buttons from Layout component
    Removed two buttons and their associated SVG icons from the layout.

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -56,16 +56,6 @@ export const Navbar: React.FC<NavbarProps> = ({ currentPage, setCurrentPage }) =
             </svg>
             Gọi Ngay
           </a>
-          <button className="p-2 text-slate-600 hover:text-amber-600">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z" />
-            </svg>
-          </button>
-          <button className="p-2 text-slate-600 hover:text-amber-600">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
-            </svg>
-          </button>
         </div>
       </div>
     </nav>
